f0634d6075bbae4a8db3ce47a23abb89020acdf9
Nantapob Vankrua <nantapob007@hotmail.com>
2023-07-27 17:56:40 +0700

    update css

--- components/modalBag.tsx
+++ components/modalBag.tsx
@@ -12,13 +12,11 @@ export default function ModalBag({ openModal, closeModal }: any) {
   return (
     <>
       <Modal
+        styles={{ modal: { background: "#b2dbbf", overflow: "auto" } }}
         open={openModal}
         onClose={() => closeModal(false)}
-        center
-        classNames={{ modal: "customModal" }}>
-        <div
-          className="mt-5 px-5 overflow-y-auto"
-          style={{ maxHeight: "500px" }}>
+        center>
+        <div className="mt-5 px-5 " style={{ maxHeight: "500px" }}>
           <p className="text-2xl font-bold text-center">Your Bag</p>
           <div className="grid gap-5 mt-5 pb-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
             {bag.map((item: any) => (
